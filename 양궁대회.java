b23ec5b2b1b3f8681ad469fd82fe2d533f9e44d5
OhSeSol <47267633+OhSeSol@users.noreply.github.com>
2022-02-14 23:03:31 +0900

    Update 양궁대회.java

--- 양궁대회.java
+++ 양궁대회.java
@@ -14,16 +14,16 @@ class Solution {
         if(n==0){
             int sum = 0;
             for(int i=0; i<info.length; i++){
-                if(info[i]!=0&&info[i]>=result[i]) sum -= 10-i;
-                else if(info[i]<result[i]) sum+= 10-i;
+                if(info[i]!=0&&info[i]>=result[i]) sum -= 10-i; //어피치가 맞힌 화살의 수가 더 많을 때
+                else if(info[i]<result[i]) sum+= 10-i; //라이언이 맞힌 화살의 수가 더 많을 때
             }
             if(sum>maxDiff){
                 maxDiff = sum;
                 maxResult = result.clone();
-            }else if(sum==maxDiff){
+            }else if(sum==maxDiff){ //점수차가 같을 때
                 for(int i=10; i>=0; i--){
                     if(maxResult[i]!=result[i]){
-                        maxResult = (maxResult[i]>result[i]) ? maxResult : result.clone();
+                        maxResult = (maxResult[i]>result[i]) ? maxResult : result.clone(); //가장 낮은 점수를 더 많이 맞힌 경우를 선택
                     }
                 }
             }
@@ -32,7 +32,7 @@ class Solution {
         }
 
         for(int i=0; i<result.length; i++){
-            if(!visited[i]){
+            if(!visited[i]){ //
                 if(n>=info[i]+1){
                     visited[i] = true;
                     result[i] = info[i] + 1;
